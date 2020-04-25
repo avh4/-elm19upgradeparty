fbddc05bae690080b042f952babd9121ee901c1d
Aaron VonderHaar <gruen0aermel@gmail.com>
2020-04-25 13:33:41 -0700

    start-time.sh can start a paused timer

--- start-timer.sh
+++ start-timer.sh
@@ -3,5 +3,9 @@
 set -euo pipefail
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+newStart="Time.millisToPosix $(date +%s)000"
 
-sed -i "" -e "s/timer = Nothing/timer = Just (lastTime 0 0 0 0, Time.millisToPosix $(date +%s)000)/" "$SCRIPTPATH/src/Data.elm"
+# create the timer if there isn't one
+sed -i "" -e "s/timer = Nothing/timer = Just (lastTime 0 0 0 0, Nothing)/" "$SCRIPTPATH/src/Data.elm"
+# unpause the timer if there is one
+sed -i "" -e "s/timer = Just (\([^,]*\), Nothing/timer = Just (\1, Just <| $newStart/" "$SCRIPTPATH/src/Data.elm"
